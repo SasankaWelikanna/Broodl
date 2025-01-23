579d00e93a0a2d6e267f8844d46cbcb2ef769bed
SasankaWelikanna <sasankatw@gmail.com>
2025-01-23 22:31:55 +0530

    Logout function completed

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -3,7 +3,7 @@ import { Fugaz_One } from 'next/font/google';
 import React, { useEffect, useState } from 'react'
 import Calendar from './Calendar';
 import { useAuth } from '@/context/AuthContext';
-import { setDoc } from 'firebase/firestore';
+import { doc, setDoc } from 'firebase/firestore';
 import { db } from '@/firebase';
 import Loading from './Loading';
 import Login from './Login';
@@ -14,13 +14,29 @@ const fugaz = Fugaz_One({ subsets: ["latin"], weight: ['400'] });
 export default function Dashboard() {
     const { currentUser, userDataObj, setUserDataObj, loading } = useAuth()
     const [data, setData] = useState({})
+    const now = new Date()
 
     function countValues() {
+        let total_number_of_days = 0
+        let sum_moods = 0
+        for (let year in data) {
+            for (let month in data[year]) {
+                for (let day in data[year][month]) {
+                    let days_mood = data[year][month][day]
+                    total_number_of_days++
+                    sum_moods += days_mood
+                }
+            }
+        }
+        return { num_days: total_number_of_days, average_mood: sum_moods / total_number_of_days }
+    }
 
+    const statuses = {
+        ...countValues(),
+        time_remaining: `${23 - now.getHours()}H ${60 - now.getMinutes()}M`,
     }
 
     async function handleSetMood(mood) {
-        const now = new Date()
         const day = now.getDate()
         const month = now.getMonth()
         const year = now.getFullYear()
@@ -53,12 +69,6 @@ export default function Dashboard() {
         }
     }
 
-    const statuses = {
-        num_days: 14,
-        time_remaining: '13:14:26',
-        date: (new Date()).toDateString()
-    }
-
     const moods = {
         '&*@#$': '😭',
         'Sad': '😢',
@@ -89,8 +99,8 @@ export default function Dashboard() {
                 {Object.keys(statuses).map((status, statusIndex) => {
                     return (
                         <div key={statusIndex} className=' flex flex-col gap-1 sm:gap-2'>
-                            <p className='font-medium uppercase text-xs sm:text-sm truncate'>{status.replaceAll('_', ' ')}</p>
-                            <p className={'text-base sm:text-lg truncate ' + fugaz.className}>{statuses[status]}</p>
+                            <p className='font-medium capitalize text-xs sm:text-sm truncate'>{status.replaceAll('_', ' ')}</p>
+                            <p className={'text-base sm:text-lg truncate ' + fugaz.className}>{statuses[status]}{status === 'num_days' ? ' 🔥' : ''}</p>
                         </div>
                     )
                 })
@@ -115,7 +125,7 @@ export default function Dashboard() {
                 }
                 )}
             </div>
-            <Calendar data={data} handleSetMood={handleSetMood} />
+            <Calendar completeData={data} handleSetMood={handleSetMood} />
         </div>
     )
 }
